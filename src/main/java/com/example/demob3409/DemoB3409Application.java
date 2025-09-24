3aa67bf455d92e08d810367663de860f7a035ecd
thomas clamon <thomas.clamon@gmail.com>
2025-09-24 14:14:32 +0200

    examples

---
+++ src/main/java/com/example/demob3409/DemoB3409Application.java
@@ -0,0 +1,13 @@
+package com.example.demob3409;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoB3409Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoB3409Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demob3409/dto/AgeDto.java
@@ -0,0 +1,34 @@
+package com.example.demob3409.dto;
+
+public class AgeDto {
+
+    Integer jour;
+
+    Integer mois;
+
+    Integer annee;
+
+    public Integer getJour() {
+        return jour;
+    }
+
+    public void setJour(Integer jour) {
+        this.jour = jour;
+    }
+
+    public Integer getMois() {
+        return mois;
+    }
+
+    public void setMois(Integer mois) {
+        this.mois = mois;
+    }
+
+    public Integer getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(Integer annee) {
+        this.annee = annee;
+    }
+}
